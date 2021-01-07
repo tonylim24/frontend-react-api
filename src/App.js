a84b83f4ff58b12ef6ca85a2f096d03ee5d0bb32
Tony Lim <tonolim24@gmail.com>
2021-01-07 14:36:13 -0800

    Finished all parts.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StudentsList from './components/StudentsList';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentsList />
+    </div>
+  );
+}
+
+export default App;
